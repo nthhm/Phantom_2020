c42dbe59c057f0add55b1dd0ef06a40eacc89c4a
nthhm <numasawa3228@gmail.com>
2020-01-07 12:58:51 +0900

    前ファイルの削除

--- Phantom(program)/Phantom/FontSize.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-int ChangeFontSize(int Size);
--- Phantom(program)/Phantom/Game.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void Game_Initialize();//‰Šú‰»
-void Game_Finalize();//I—¹ˆ—
-void Game_Update();//XV
-void Game_Draw();//•`‰æ
